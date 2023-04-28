c383f61989d127c924bcfa8f8d61adf77f933bbc
Numnzel <ionikshockwave@gmail.com>
2023-04-28 16:59:06 +0200

    Added planet shader
    - Added planet shader.

---
+++ SpaceCourier/Assets/Scripts/Planet.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+    public float rotationSpeed;
+	//private MaterialPropertyBlock materialPropertyBlock;
+	[SerializeField] private Renderer meshRenderer;
+
+	private void Awake() {
+
+		//SetShader();
+		InitialInclination();
+	}
+
+	private void Update() {
+
+		Rotate();
+	}
+
+	/* private void SetShader() {
+
+		materialPropertyBlock = new();
+		meshRenderer.GetPropertyBlock(materialPropertyBlock);
+
+		int property = Shader.PropertyToID("_Seed");
+		float seed = (transform.position.x + transform.position.z) / 10.0f;
+		
+		materialPropertyBlock.SetFloat(property, seed);
+		meshRenderer.SetPropertyBlock(materialPropertyBlock);
+	} */
+
+	private void InitialInclination() {
+
+		float maxDegrees = (1.0f + Mathf.Sin(transform.position.x)) + (1.0f + Mathf.Cos(transform.position.z)) * 5.0f;
+
+		Vector3 inclination = new Vector3(transform.localRotation.x, transform.localRotation.y + Random.Range(0, 360f), transform.localRotation.z + Random.Range(0, maxDegrees));
+		
+		transform.localRotation = Quaternion.Euler(inclination);
+	}
+
+	private void Rotate() {
+
+		Vector3 rotationValue = new Vector3(0, rotationSpeed * Time.deltaTime, 0);
+
+		transform.Rotate(rotationValue);
+	}
+}
